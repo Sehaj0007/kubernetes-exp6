cee94371beb8f8cbe850a46728b0862b359e49ab
sehaj0007 <kaursehajpreet0728@gmail.com>
2026-04-07 14:13:56 +0530

    this commit is made to inititate the github actions workflow

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const http = require('http');
 const server = http.createServer((req, res) => {
-  res.end("Hello from CI/CD + GitOps 🚀");
+  res.end("Hello from CI/CD + GitOps ! 🚀");
 });
 server.listen(3000);
